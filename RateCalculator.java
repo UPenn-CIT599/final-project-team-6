0f5527674d605f31f1f5bd4be10b3526183a70d3
kcharl-upenn <62820812+kcharl-upenn@users.noreply.github.com>
2020-04-12 19:39:29 -0400

    Add files via upload
    update JavaDoc comments

--- RateCalculator.java
+++ RateCalculator.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.*;
 
+/**
+ * This class contains a set of basic math functions for obtaining the electricity and gasoline-related values called in select formula.
+ *
+ */
 public class RateCalculator {
 
-	// set the output to print 2 digits only
-	private static DecimalFormat df2 = new DecimalFormat("#.##");
 	/**
+	 * Sets the output to print 2 digits only.
+	 */
+	private static DecimalFormat df2 = new DecimalFormat("#.##");
+	/*
 	 * Initialize several instance variables, which can be used in other classes
 	 * EVMPKwh = EV's miles / kwh. CharlotteERate = 6 months Charlotte's average
 	 * electricity rate in dollar. NewYorkERate = 3 year New York's average
@@ -27,10 +33,9 @@ public class RateCalculator {
 	double hwyPercentage = 0.0;
 
 	/**
-	 * This method will take 2 arraylists (before EV bill and after EV bill) to find
-	 * out how many electricity will consume in 6 months Then use total driving
-	 * miles in 6 months / total consume electricity and return mile / kwh
-	 * 
+	 * This method will take two arraylists (before EV bill and after EV bill) to find
+	 * how much electricity will be consumed in 6 months. Then calculate total driving
+	 * miles in 6 months / total consume electricity and return mile / kwh.
 	 * @param ElectricityUsageBeforeEV
 	 * @param ElectricityUsageAfterEV
 	 * @return mile / kwh
@@ -40,16 +45,14 @@ public class RateCalculator {
 		int sumOfDifference = 0;
 		for (int i = 0; i < ElectricityUsageBeforeEV.size(); i++) {
 			sumOfDifference = sumOfDifference + ElectricityUsageAfterEV.get(i) - ElectricityUsageBeforeEV.get(i);
-		}
+	}
 		EVMPkwh = totalMilesForSixMonths / sumOfDifference;
 		return EVMPkwh;
 	}
-
 	/**
 	 * This method will take 2 arraylists (after EV usage and after EV cost) to find
-	 * out how many electricity will consume in 6 months and how much dollar cost.
-	 * Then return Charlotte average $ / kwh.
-	 * 
+	 * how much electricity will be consumed in 6 months, and the associated cost.
+	 * Return Charlotte average $ / kwh.
 	 * @param ElectricityUsageCost
 	 * @param ElectricityUsageAfterEV
 	 * @return Charlotte average $ / kwh
@@ -65,33 +68,29 @@ public class RateCalculator {
 		CharlotteERate = sumOfCost / sumOfUsage;
 		return CharlotteERate;
 	}
-
 	/**
-	 * @return average electricity cost for 3 years for New York
+	 * Returns the average electricity cost for 3 years in New York, NY.
 	 */
-	public double NewYorkElectrcityRate() {
+	public double NewYorkElectricityRate() {
 		double NewYorkERate2017 = 0.1803;
 		double NewYorkERate2018 = 0.21;
 		double NewYorkERate2019 = 0.201;
 		NewYorkERate = (NewYorkERate2017 + NewYorkERate2018 + NewYorkERate2019) / 3;
 		return NewYorkERate;
 	}
-
 	/**
-	 * @return average electricity cost for 3 years for Miami.
+	 * Returns the average electricity cost for 3 years in Miami, FL.
 	 */
-	public double MiamiElectrcityRate() {
+	public double MiamiElectricityRate() {
 		double MiamiERate2017 = 0.119;
 		double MiamiERate2018 = 0.119;
 		double MiamiERate2019 = 0.121;
 		MiamiERate = (MiamiERate2017 + MiamiERate2018 + MiamiERate2019) / 3;
 		return MiamiERate;
 	}
-
 	/**
-	 * This method get inputs from user and return the data comparison between
-	 * Tesla, Mercedes,and Lexus in Charlotte, New York, Miami.
-	 * 
+	 * This method retrieves input from a user and returns the data to compare
+	 * the Tesla, Mercedes,and Lexus vehicles in Charlotte, New York, and Miami.
 	 * @param miles
 	 * @param year
 	 * @param hwyPercentage
@@ -102,7 +101,7 @@ public class RateCalculator {
 	double NewYorkUserYearEV;
 	double MiamiOneYearEV;
 	double MiamiUserYearEV;
-
+	
 	public void CostComparsion(double miles, double year, double hwyPercentage) {
 		this.miles = miles;
 		this.year = year;
@@ -126,7 +125,6 @@ public class RateCalculator {
 		double MiamiOneYearMercedes = (miles * hwyPercentage) / icedata.MercedesHwyMPG * icedata.MiamiPremiumGas
 				+ (miles * (1 - hwyPercentage)) / icedata.MercedesCityMPG * icedata.MiamiPremiumGas;
 		double MiamiUserYearMercedes = MiamiOneYearMercedes * year;
-
 		double CharlotteOneYearLexus = (miles * hwyPercentage) / icedata.LexusHwyMPG * icedata.CharlotteRegularGas
 				+ (miles * (1 - hwyPercentage)) / icedata.LexusCityMPG * icedata.CharlotteRegularGas;
 		double CharlotteUserYearLexus = CharlotteOneYearLexus * year;
@@ -136,13 +134,13 @@ public class RateCalculator {
 		double MiamiOneYearLexus = (miles * hwyPercentage) / icedata.LexusHwyMPG * icedata.MiamiRegularGas
 				+ (miles * (1 - hwyPercentage)) / icedata.LexusCityMPG * icedata.MiamiRegularGas;
 		double MiamiUserYearLexus = MiamiOneYearLexus * year;
-
-		// calculate the max saving and city compare driving EV and ICE.
+		/*
+		 * Calculates the max saving and city comparisons for driving the Tesla and ICE vehicles.
+		 */
 		String[] citylist = { "Charlotte", "New York", "Miami" };
 		double[][] comparelist = { { CharlotteOneYearEV, CharlotteOneYearMercedes, CharlotteOneYearLexus },
 				{ NewYorkOneYearEV, NewYorkOneYearMercedes, NewYorkOneYearLexus },
 				{ MiamiOneYearEV, MiamiOneYearMercedes, MiamiOneYearLexus } };
-
 		double temp = 0.0;
 		double maxSaving = 0.0;
 		int maxIndex = 0;
@@ -151,10 +149,9 @@ public class RateCalculator {
 			maxSaving = temp;
 			maxIndex = i;
 		}
-
-		/**
-		 * Use national average data to compare tesla,mercedes and lexus cost for 15,000
-		 * miles/
+		/*
+		 * Uses national average data to compare the Tesla, and both the Mercedes GLC and Lexus LX
+		 * cost for 15,000 miles driven annually.
 		 */
 		double threeYearAverageCostOfPremium = 3.07;
 		double currentNationalCostOfRegular = 2.24;
@@ -162,39 +159,34 @@ public class RateCalculator {
 		double averageMilesDrivenAnnually = 15000;
 		double currentNationalCostOfElectricity = 0.1279;
 		double teslAverageUsage = 3.84;
-
 		double TeslaCost = averageMilesDrivenAnnually / teslAverageUsage * currentNationalCostOfElectricity;
 		double MercedesCost = averageMilesDrivenAnnually / combinedMpg * threeYearAverageCostOfPremium;
 		double LexusCost = averageMilesDrivenAnnually / combinedMpg * currentNationalCostOfRegular;
 
-		/**
-		 * Answer some questions and output answers in one txt file.
+		/*
+		 * Answers select questions and outputs answers in .txt file.
 		 */
-
 		ArrayList<String> answers = new ArrayList<>();
-
 		answers.add("If you live in Charlotte, Tesla Model 3 will cost you $" + df2.format(CharlotteOneYearEV)
 				+ " in 1 year; $" + df2.format(CharlotteUserYearEV) + " for " + year + " years.");
 		answers.add("If you live in Charlotte, Lexus will cost you $" + df2.format(CharlotteOneYearLexus)
 				+ " in 1 year; $" + df2.format(CharlotteUserYearLexus) + " for " + year + " years.");
-		answers.add("If you live in Charlotte, Mercede will cost you $" + df2.format(CharlotteOneYearMercedes)
+		answers.add("If you live in Charlotte, Mercedes will cost you $" + df2.format(CharlotteOneYearMercedes)
 				+ " in 1 year; $" + df2.format(CharlotteUserYearMercedes) + " for " + year + " years.");
 		answers.add("If you live in New York, Tesla Model 3 will cost you $" + df2.format(NewYorkOneYearEV)
 				+ " in 1 year; $" + df2.format(NewYorkUserYearEV) + " for " + year + " years.");
 		answers.add("If you live in New York, Lexus will cost you $" + df2.format(NewYorkOneYearLexus) + " in 1 year; $"
 				+ df2.format(NewYorkUserYearLexus) + " for " + year + " years.");
-		answers.add("If you live in New York, Mercede will cost you $" + df2.format(NewYorkOneYearMercedes)
+		answers.add("If you live in New York, Mercedes will cost you $" + df2.format(NewYorkOneYearMercedes)
 				+ " in 1 year; $" + df2.format(NewYorkUserYearMercedes) + " for " + year + " years.");
 		answers.add("If you live in Miami, Tesla Model 3 will cost you $" + df2.format(MiamiOneYearEV) + " in 1 year; $"
 				+ df2.format(MiamiUserYearEV) + " for " + year + " years.");
 		answers.add("If you live in Miami, Lexus will cost you $" + df2.format(MiamiOneYearLexus) + " in 1 year; $"
 				+ df2.format(MiamiUserYearLexus) + " for " + year + " years.");
-		answers.add("If you live in Miami, Mercede will cost you $" + df2.format(MiamiOneYearMercedes) + " in 1 year; $"
+		answers.add("If you live in Miami, Mercedes will cost you $" + df2.format(MiamiOneYearMercedes) + " in 1 year; $"
 				+ df2.format(MiamiUserYearMercedes) + " for " + year + " years.");
-		answers.add("\n");
 		answers.add("If you drive Tesla, you will save the most in " + citylist[maxIndex] + " city, total saving is $"
 				+ df2.format(maxSaving) + " in 1 year.");
-
 		answers.add("\n");
 		answers.add("In comparsion, the national data show the average cost in 1 year:");
 		answers.add("Tesla Model 3 will cost $" + df2.format(TeslaCost) + ".");
@@ -203,79 +195,44 @@ public class RateCalculator {
 		answers.add("Tesla will save " + df2.format(100 * (1 - (TeslaCost / MercedesCost)))
 				+ "% compare with Mercedes Benz.");
 		answers.add("Tesla will save " + df2.format(100 * (1 - (TeslaCost / LexusCost))) + "% compare with Lexus.");
-
+		
 		FormattedOutput outputFile = new FormattedOutput(answers);
 		outputFile.writeAnswers();
 	}
 	/**
-	 * getter methods for all EV rate in 3 different cities.
-	 * @return
+	 * Retrieves Charlotte EV rate for one year.
 	 */
 	public double getCharlotteOneYearEV() {
 		return CharlotteOneYearEV;
 	}
-
+	/**
+	 * Retrieves Charlotte EV user rate.
+	 */
 	public double getCharlotteUserYearEV() {
 		return CharlotteUserYearEV;
 	}
-
+	/**
+	 * Retrieves New York EV rate for one year.
+	 */
 	public double getNewYorkOneYearEV() {
 		return NewYorkOneYearEV;
 	}
-
+	/**
+	 * Retrieves New York EV user rate.
+	 */
 	public double getNewYorkUserYearEV() {
 		return NewYorkUserYearEV;
 	}
-
+	/**
+	 * Retrieves Miami EV rate for one year.
+	 */
 	public double getMiamiOneYearEV() {
 		return MiamiOneYearEV;
 	}
-
+	/**
+	 * Retrieves Miami EV user rate.
+	 */
 	public double getMiamiUserYearEV() {
 		return MiamiUserYearEV;
 	}
-
-	ArrayList<Double> nonEVPlot = new ArrayList<Double>();
-	ArrayList<Double> EVPlot = new ArrayList<Double>();
-	/**
-	 * This method will take user's input of month, and return 2 arraylists which contain all daily cost of that particular month before and after EV charge.
-	 * @param month = user's input of month
-	 */
-	public void monthofPlot(String month) {
-
-		ElectricUsageReader usageWithoutEV = new ElectricUsageReader("09-2018--03-2019.csv");
-		ElectricUsageReader usageWithEV = new ElectricUsageReader("09-2019--03-2020.csv");
-		usageWithoutEV.list();
-		usageWithEV.list();
-		usageWithoutEV.eachMonthlyCost();
-		usageWithEV.eachMonthlyCost();
-
-		String lowerCasemonth = month.toLowerCase();
-
-		if (lowerCasemonth.equals("sep")) {
-			nonEVPlot = usageWithoutEV.getSepCost();
-			EVPlot = usageWithEV.getSepCost();
-		}
-		if (lowerCasemonth.equals("oct")) {
-			nonEVPlot = usageWithoutEV.getOctCost();
-			EVPlot = usageWithEV.getOctCost();
-		}
-		if (lowerCasemonth.equals("nov")) {
-			nonEVPlot = usageWithoutEV.getNovCost();
-			EVPlot = usageWithEV.getNovCost();
-		}
-		if (lowerCasemonth.equals("dec")) {
-			nonEVPlot = usageWithoutEV.getDecCost();
-			EVPlot = usageWithEV.getDecCost();
-		}
-		if (lowerCasemonth.equals("jan")) {
-			nonEVPlot = usageWithoutEV.getJanCost();
-			EVPlot = usageWithEV.getJanCost();
-		}
-		if (lowerCasemonth.equals("feb")) {
-			nonEVPlot = usageWithoutEV.getFebCost();
-			EVPlot = usageWithEV.getFebCost();
-		}
-	}
-
 }
