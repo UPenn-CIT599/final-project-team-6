a307cf6108b62a7371f8323e15f3e44fb6087f09
kcharl-upenn <62820812+kcharl-upenn@users.noreply.github.com>
2020-04-15 16:18:45 -0400

    Add files via upload

---
+++ src/ElectricUsage.java
@@ -0,0 +1,40 @@
+public class ElectricUsage {
+/**
+ * This class set all getter methods for electricity usage information
+ * month = month of the year which usage occurs.
+ * day = day of the month which usage occurs.
+ * usage = total usage in one day, unit is kwh.
+ * cost = total cost in one day, unit is $. 
+ *  
+ */
+	private int month;
+	private int day;
+	private int usage;
+	private double cost;
+	/**
+	 * Constructor for the class
+	 * @param month
+	 * @param day
+	 * @param usage
+	 * @param cost
+	 */
+	public ElectricUsage(int month, int day, int usage, double cost) {
+		this.month = month;
+		this.day = day;
+		this.usage = usage;
+		this.cost = cost;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public int getUsage() {
+		return usage;
+	}
+	public double getCost() {
+		return cost;
+	}
+}
